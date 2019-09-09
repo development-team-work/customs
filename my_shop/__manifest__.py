dd37492e6a9ec76c0d25b9fb285e213073d01f30
ashraf Uzzaman <development-team-work@gmail.com>
2019-09-10 01:24:19 +0600

    depandancy added

--- my_shop/__manifest__.py
+++ my_shop/__manifest__.py
@@ -13,7 +13,7 @@ Customisation Eagle ERP
     'website': 'http://www.eagle.com/page/billing',
     'images' : ['images/accounts.jpeg','images/bank_statement.jpeg','images/cash_register.jpeg','images/chart_of_accounts.jpeg','images/customer_invoice.jpeg','images/journal_entries.jpeg'],
     'depends' : ['base','website','stock','account','sale_management','point_of_sale',
-                 'note','website_sale','purchase','contacts','mrp','l10n_bd'],
+                 'note','website_sale','accounting_pdf_reports','purchase','contacts','mrp','l10n_bd'],
     'data': [
         # 'data/payment_acquirer.xml',
         'reports/product_label.xml',
