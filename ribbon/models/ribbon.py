095b7985f09d909a0dcaa799192d77feaa60e2db
Khanstore <dmashraf@gmail.com>
2020-05-01 04:29:23 +0600

    Image for police Unit added

--- ribbon/models/ribbon.py
+++ ribbon/models/ribbon.py
@@ -46,12 +46,35 @@ class ribbonMedalForceUnit(models.Model):
     _name = "ribbon.force.unit"
     _description = "Name of Units of a Force"
     name = fields.Char("Unit",translate=True)
-    image=fields.Boolean("Div Sign")
+    divsign=fields.Binary("Div Sign")
+    image=fields.Binary("Div Sign", compute='_compute_image_1920', inverse='_set_image_1920')
     force_name=fields.Many2one("ribbon.force","Force")
     address=fields.Char("address",translate=True)
     parent_unit=fields.Many2one("ribbon.force.unit","Parent Unit",translate=True)
     chief_rank=fields.Many2one("ribbon.rank","Rank of Chief",translate=True)
     chief=fields.Many2one("ribbon.post","Chief",translate=True)
+
+    street = fields.Char('Street')
+    street2 = fields.Char('Street2')
+    zip = fields.Char('Post Code', change_default=True)
+    city = fields.Char('City')
+    state_id = fields.Many2one("res.country.state", string='State')
+    country_id = fields.Many2one('res.country', string='Country')
+    phone = fields.Char('Phone', tracking=50)
+    mobile = fields.Char('Mobile')
+
+    def _compute_image_1920(self):
+        """Get the image from the template if no image is set on the variant."""
+        for record in self:
+            if record.divsign==False:
+                record.image =  record.parent_unit.image
+            else:
+                record.image = record.divsign
+
+    def _set_image_1920(self):
+        for rec in self:
+            rec.divsign=rec.image
+
 
 class RibbonMedalsPost(models.Model):
     _name="ribbon.post"
