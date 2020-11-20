10058a70172deaf5b8e0d803088447ef7e1c9f09
Khanstore <dmashraf@gmail.com>
2020-11-20 15:55:03 +0600

    l10n_bd perfect
    just copied from l10n_uk

--- l10n_bd/__init__.py
+++ l10n_bd/__init__.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, SUPERUSER_ID
 
-from . import models
-
-def load_translations(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env.ref('l10n_bd.l10nbd_chart_template').process_coa_translations()
