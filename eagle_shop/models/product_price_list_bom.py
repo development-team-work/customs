aaf4520ddb320662ed9ad9f147fe81ae1ad1f1a9
Khanstore <dmashraf@gmail.com>
2021-01-05 17:41:29 +0600

    Update product_price_list_bom.py

--- eagle_shop/models/product_price_list_bom.py
+++ eagle_shop/models/product_price_list_bom.py
@@ -20,26 +20,26 @@ class productPriceListItem(models.Model):
         ('bom', 'Bill of material (BOM)'),
         ('formula', 'Formula')], index=True, default='fixed')
 
-    @api.depends('categ_id', 'product_tmpl_id', 'product_id', 'compute_price', 'fixed_price', \
-        'pricelist_id', 'percent_price', 'price_discount', 'price_surcharge')
-    def _get_pricelist_item_name_price(self):
-        if self.categ_id:
-            self.name = _("Category: %s") % (self.categ_id.name)
-        elif self.product_tmpl_id:
-            self.name = self.product_tmpl_id.name
-        elif self.product_id:
-            self.name = self.product_id.display_name.replace('[%s]' % self.product_id.code, '')
-        else:
-            self.name = _("All Products")
-
-        if self.compute_price == 'fixed':
-            self.price = ("%s %s") % (self.fixed_price, self.pricelist_id.currency_id.name)
-        elif self.compute_price == 'percentage':
-            self.price = _("%s %% discount") % (self.percent_price)
-        elif self.compute_price == 'bom':
-            self.price = self.bom_price
-        else:
-            self.price = _("%s %% discount and %s surcharge") % (self.price_discount, self.price_surcharge)
+    # @api.depends('categ_id', 'product_tmpl_id', 'product_id', 'compute_price', 'fixed_price', \
+    #     'pricelist_id', 'percent_price', 'price_discount', 'price_surcharge')
+    # def _get_pricelist_item_name_price(self):
+    #     if self.categ_id:
+    #         self.name = _("Category: %s") % (self.categ_id.name)
+    #     elif self.product_tmpl_id:
+    #         self.name = self.product_tmpl_id.name
+    #     elif self.product_id:
+    #         self.name = self.product_id.display_name.replace('[%s]' % self.product_id.code, '')
+    #     else:
+    #         self.name = _("All Products")
+    #
+    #     if self.compute_price == 'fixed':
+    #         self.price = ("%s %s") % (self.fixed_price, self.pricelist_id.currency_id.name)
+    #     elif self.compute_price == 'percentage':
+    #         self.price = _("%s %% discount") % (self.percent_price)
+    #     elif self.compute_price == 'bom':
+    #         self.price = self.bom_price
+    #     else:
+    #         self.price = _("%s %% discount and %s surcharge") % (self.price_discount, self.price_surcharge)
 
     def _get_pricelist_item_component_price(self,kit):
 
@@ -58,20 +58,20 @@ class ProductPriceListIterms(models.Model):
     _inherit = "product.pricelist.item"
 
 
-    @api.onchange('applied_on')
-    def _onchange_applied_on(self):
-        if self.applied_on != '0_product_variant':
-            self.product_id = False
-#todo correct following line
-            # self.product_tmpl_id=context['default_product_id'}
-        if self.applied_on != '1_product':
-            self.product_tmpl_id = False
-        if self.applied_on != '2_product_category':
-            self.categ_id = False
-        if self.applied_on == '0_product_variant':
-            self.product_id = self._context['default_product_id']
-        if self.applied_on == 'product_tmpl_id':
-            self.product_tmpl_id = self._context['default_product_tmpl_id']
+#     @api.onchange('applied_on')
+#     def _onchange_applied_on(self):
+#         if self.applied_on != '0_product_variant':
+#             self.product_id = False
+# #todo correct following line
+#             # self.product_tmpl_id=context['default_product_id'}
+#         if self.applied_on != '1_product':
+#             self.product_tmpl_id = False
+#         if self.applied_on != '2_product_category':
+#             self.categ_id = False
+#         if self.applied_on == '0_product_variant':
+#             self.product_id = self._context['default_product_id']
+#         if self.applied_on == 'product_tmpl_id':
+#             self.product_tmpl_id = self._context['default_product_tmpl_id']
 
 class productPricePerPriceList(models.Model):
     _name="product.pricelist.price"
